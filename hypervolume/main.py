219f971f7310eba60399784b273e191eb02f663f
jeremyy55 <jeremy-y@live.be>
2019-01-10 18:09:13 +0100

    la fonction pour lire les fichier de solution pour l'hypervolumes marchent

--- hypervolume/main.py
+++ hypervolume/main.py
@@ -4,6 +4,21 @@ import numpy as np
 from pprint import pprint
 
 
+def read_data_in_solution_file(filename, dir_path='../nos_solutions/'):
+    with open(dir_path+filename, 'r') as f:
+        all_data = f.read().split('\n')
+        file_len = len(all_data)-1
+        xs = []
+        for i in range(int(file_len/2)):
+            xs.append(all_data[i].split(' ')[:-1])
+        #print('xs:\n', xs)
+        sols = []
+        for i in range(int(file_len/2), file_len):
+            sols.append(all_data[i].split(' ')[:-1])
+        #print('sols:\n', sols)
+    return xs, sols
+
+
 def bi_objectif():
     archive = [[2, 4], [3, 3], [5, 2]]
     reference = [7, 5]
@@ -18,4 +33,6 @@ def tri_objectif():
 
 if __name__ == '__main__':
     print('debut')
-    tri_objectif()
+    # tri_objectif()
+    filename = 'trentex30_2obj.txt'
+    read_data_in_solution_file(filename)
